2698d3a81c6b5eecb87af99cf28f146d1f82455c
Andrei Sabau <hello@andreisabau.com>
2018-11-26 03:49:26 +0200

    Added solution

---
+++ Navtrack.Listener/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Navtrack.Listener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
