9dbce2e29552e92c616822191d78b5dc9fcbbb62
Andrei Sabau <hello@andreisabau.com>
2019-06-25 03:58:44 +0300

    Update ValuesController.cs

--- Navtrack.Api/Controllers/ValuesController.cs
+++ Navtrack.Api/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Navtrack.Api.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] {"value1", "value2"};
+            return new[] {"value1", "value2", "value3"};
         }
 
         // GET api/values/5
