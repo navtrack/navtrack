c3f6737fd32eab37f92cbb32aab9e2fc262972a7
Andrei Sabau <hello@andreisabau.com>
2019-08-03 02:33:59 +0300

    Create TestController.cs

---
+++ Navtrack.Api/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Navtrack.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        public TestController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            string connectionString = configuration.GetConnectionString("navtrack");
+
+            return connectionString;
+        }
+    }
+}
