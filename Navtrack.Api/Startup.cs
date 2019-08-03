179c2fb8ce725f6f9d0934c115677d0d0611c458
Andrei Sabau <hello@andreisabau.com>
2019-08-04 02:13:26 +0300

    Added locations controller

--- Navtrack.Api/Startup.cs
+++ Navtrack.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Navtrack.Library.DI;
 
 namespace Navtrack.Api
 {
@@ -18,6 +19,7 @@ namespace Navtrack.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Bootstrapper.ConfigureServices(services);
             services.AddControllers();
         }
 
