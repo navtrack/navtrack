bbf8a27159673922352c10a8a7a9ce3e54cc7eb3
Andrei Sabau <hello@andreisabau.com>
2019-08-06 00:10:17 +0300

    Added initial migration

--- Navtrack.DataAccess/Migrations/DesignTimeDbContextFactory.cs
+++ Navtrack.DataAccess/Migrations/DesignTimeDbContextFactory.cs
@@ -1,27 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
-using Microsoft.Extensions.Configuration;
 using Navtrack.DataAccess.Model;
 
 namespace Navtrack.DataAccess.Migrations
 {
     public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<NavtrackContext>
     {
-        private readonly IConfiguration configuration;
-
-        public DesignTimeDbContextFactory(IConfiguration configuration)
-        {
-            this.configuration = configuration;
-        }
-
         public NavtrackContext CreateDbContext(string[] args)
         {
             DbContextOptionsBuilder<NavtrackContext> optionsBuilder =
                 new DbContextOptionsBuilder<NavtrackContext>();
 
-            string connectionString = configuration.GetConnectionString("navtrack");
-            
-            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.UseSqlServer(
+                "data source=localhost;initial catalog=navtrack;user id=navtrack;password=navtrack;");
 
             return new NavtrackContext(optionsBuilder.Options);
         }
