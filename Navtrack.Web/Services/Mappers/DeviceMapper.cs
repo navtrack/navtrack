58e771b93c4fa35867a32b4cb9413bee4bf978f2
Andrei Sabau <hello@andreisabau.com>
2019-10-05 13:10:50 +0300

    Create DeviceMapper.cs

---
+++ Navtrack.Web/Services/Mappers/DeviceMapper.cs
@@ -0,0 +1,19 @@
+using Navtrack.Library.DI;
+using Navtrack.Library.Services;
+using Navtrack.Web.Models;
+
+namespace Navtrack.Web.Services.Mappers
+{
+    [Service(typeof(IMapper<DataAccess.Model.Device, Device>))]
+    public class DeviceMapper : IMapper<DataAccess.Model.Device, Device>
+    {
+        public Device Map(DataAccess.Model.Device source, Device destination)
+        {
+            destination.Id = source.Id;
+            destination.Name = source.Object.Name;
+            destination.IMEI = destination.IMEI;
+
+            return destination;
+        }
+    }
+}
