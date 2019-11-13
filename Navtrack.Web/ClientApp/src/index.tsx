734073a979501bcfd4315241f393dac075926a96
Andrei Sabau <hello@andreisabau.com>
2019-11-13 02:03:34 +0200

    Added missing packages

--- Navtrack.Web/ClientApp/src/index.tsx
+++ Navtrack.Web/ClientApp/src/index.tsx
@@ -5,11 +5,10 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <BrowserRouter basename={baseUrl}>
+  <BrowserRouter>
     <App />
   </BrowserRouter>,
   rootElement);
