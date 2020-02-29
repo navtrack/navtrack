2623cba3a671be275229a7dc162b1805abf35699
Andrei Sabau <hello@andreisabau.com>
2020-02-29 21:51:23 +0200

    Create ValidationKeysStore.cs

---
+++ Navtrack.Web/Services/IdentityServer/ValidationKeysStore.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
+using Microsoft.IdentityModel.Tokens;
+using Navtrack.Library.DI;
+using Navtrack.Web.Services.LetsEncrypt;
+
+namespace Navtrack.Web.Services.IdentityServer
+{
+    [Service(typeof(IValidationKeysStore))]
+    public class ValidationKeysStore : IValidationKeysStore
+    {
+        private readonly ICertificateProvider certificateProvider;
+
+        public ValidationKeysStore(ICertificateProvider certificateProvider)
+        {
+            this.certificateProvider = certificateProvider;
+        }
+
+        public async Task<IEnumerable<SecurityKeyInfo>> GetValidationKeysAsync()
+        {
+            X509Certificate2 certificate = await certificateProvider.GetCertificate();
+
+            return certificate != null
+                ? new[]
+                {
+                    new SecurityKeyInfo
+                    {
+                        Key = new X509SecurityKey(certificate, SecurityAlgorithms.RsaSha256),
+                        SigningAlgorithm = SecurityAlgorithms.RsaSha256
+                    }
+                }
+                : Enumerable.Empty<SecurityKeyInfo>();
+        }
+    }
+}
