a5cca0cae8b6ad30c2f8470c7d83fe325c6de5ff
Andrei Sabau <hello@andreisabau.com>
2020-05-10 00:22:24 +0300

    Add iTracGPS protocol

---
+++ Navtrack.Listener/Protocols/iTracGPS/iTracGPSMessageHandler.cs
@@ -0,0 +1,12 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Protocols.TkStar;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.iTracGPS
+{
+    [Service(typeof(ICustomMessageHandler<iTracGPSProtocol>))]
+    // ReSharper disable once InconsistentNaming
+    public class iTracMessageHandler : TkStarMessageHandler
+    {
+    }
+}
---
+++ Navtrack.Listener/Protocols/iTracGPS/iTracGPSProtocol.cs
@@ -0,0 +1,13 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Protocols.TkStar;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.iTracGPS
+{
+    [Service(typeof(IProtocol))]
+    // ReSharper disable once InconsistentNaming
+    public class iTracGPSProtocol : TkStarProtocol
+    {
+        public override int Port => 7027;
+    }
+}
