676b19d0178e90d15867f15a69e5f051acac7636
Andrei Sabau <hello@andreisabau.com>
2020-05-10 01:30:45 +0300

    Update QueclinkProtocolTests.cs

--- Navtrack.Listener.Tests/Protocols/Queclink/QueclinkProtocolTests.cs
+++ Navtrack.Listener.Tests/Protocols/Queclink/QueclinkProtocolTests.cs
@@ -28,5 +28,21 @@ namespace Navtrack.Listener.Tests.Protocols.Queclink
 
             Assert.IsNotNull(ProtocolTester.LastParsedLocation);
         }
+        
+        [Test]
+        public void DeviceSendsLocationV4_ParsedLocationIsNotNull()
+        {
+            ProtocolTester.SendStringFromDevice("+RESP:GTSPD,020102,135790246811220,,0,0,1,1,4.3,92,70.0,121.354335,31.222073,20090214013254,0460,0000,18d8,6141,00,,20090214093254,11F0$");
+
+            Assert.IsNotNull(ProtocolTester.LastParsedLocation);
+        }
+        
+        [Test]
+        public void DeviceSendsLocationV5_ParsedLocationIsNotNull()
+        {
+            ProtocolTester.SendStringFromDevice("+RESP:GTRTL,020102,135790246811220,,0,0,1,1,4.3,92,70.0,121.354335,31.222073,20090214013254,0460,0000,18d8,6141,00,,20090214093254,11F0$");
+
+            Assert.IsNotNull(ProtocolTester.LastParsedLocation);
+        }
     }
 }
