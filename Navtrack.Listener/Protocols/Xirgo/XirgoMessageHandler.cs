317057de58f51646b616eb1813fd225bcd373fc7
Andrei Sabau <hello@andreisabau.com>
2020-05-09 20:11:56 +0300

    Add Xirgo protocol

---
+++ Navtrack.Listener/Protocols/Xirgo/XirgoMessageHandler.cs
@@ -0,0 +1,47 @@
+using System;
+using Navtrack.Library.DI;
+using Navtrack.Listener.Helpers;
+using Navtrack.Listener.Models;
+using Navtrack.Listener.Server;
+using static System.String;
+
+namespace Navtrack.Listener.Protocols.Xirgo
+{
+    [Service(typeof(ICustomMessageHandler<XirgoProtocol>))]
+    public class XirgoMessageHandler : BaseMessageHandler<XirgoProtocol>
+    {
+        public override Location Parse(MessageInput input)
+        {
+            Location location = new Location
+            {
+                Device = new Device
+                {
+                    IMEI = input.DataMessage.CommaSplit[0].Replace("$", Empty)
+                },
+                DateTime = DateTime.Parse($"{input.DataMessage.CommaSplit[2]} {input.DataMessage.CommaSplit[3]}"),
+                Latitude = input.DataMessage.CommaSplit.Get<decimal>(4),
+                Longitude = input.DataMessage.CommaSplit.Get<decimal>(5),
+                Altitude = input.DataMessage.CommaSplit.Get<decimal?>(6),
+                Speed = input.DataMessage.CommaSplit.Get<decimal?>(7),
+            };
+
+
+            if (input.DataMessage.CommaSplit.Length >= 24)
+            {
+                location.Heading = input.DataMessage.CommaSplit.Get<decimal?>(11);
+                location.Satellites = input.DataMessage.CommaSplit.Get<short?>(12);
+                location.HDOP = input.DataMessage.CommaSplit.Get<decimal?>(13);
+                location.Odometer = input.DataMessage.CommaSplit.Get<double?>(14);
+            }
+            else
+            {
+                location.Heading = input.DataMessage.CommaSplit.Get<decimal?>(8);
+                location.Satellites = input.DataMessage.CommaSplit.Get<short?>(9);
+                location.HDOP = input.DataMessage.CommaSplit.Get<decimal?>(10);
+                location.Odometer = input.DataMessage.CommaSplit.Get<double?>(13);
+            }
+
+            return location;
+        }
+    }
+}
---
+++ Navtrack.Listener/Protocols/Xirgo/XirgoProtocol.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Xirgo
+{
+    [Service(typeof(IProtocol))]
+    public class XirgoProtocol : BaseProtocol
+    {
+        public override int Port => 7024;
+        public override byte[] MessageStart => new byte[] {0x24, 0x24};
+        public override IEnumerable<byte[]> MessageEnd => new List<byte[]> {new byte[] {0x23, 0x23}};
+    }
+}
