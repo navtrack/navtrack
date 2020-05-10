8716ddadadba8eed15f2f351fb088b372d94b714
Andrei Sabau <hello@andreisabau.com>
2020-05-10 14:10:46 +0300

    Add Pretrace protocol

---
+++ Navtrack.Listener/Protocols/Pretrace/PretraceProtocol.cs
@@ -0,0 +1,12 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Pretrace
+{
+    [Service(typeof(IProtocol))]
+    public class PretraceProtocol : BaseProtocol
+    {
+        public override int Port => 7030;
+        public override byte[] MessageStart => new byte[] {0x24};
+    }
+}
