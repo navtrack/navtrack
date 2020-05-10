403bad7e17ea9e27199023d4ea29290fc7355e6d
Andrei Sabau <hello@andreisabau.com>
2020-05-10 17:10:56 +0300

    Update HaicomProtocol.cs

--- Navtrack.Listener/Protocols/Haicom/HaicomProtocol.cs
+++ Navtrack.Listener/Protocols/Haicom/HaicomProtocol.cs
@@ -7,5 +7,6 @@ namespace Navtrack.Listener.Protocols.Haicom
     public class HaicomProtocol : BaseProtocol
     {
         public override int Port => 7032;
+        public override byte[] MessageStart => new byte[] {0x24};
     }
 }
