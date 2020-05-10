6d71f5acb9cb14ac5e4e0402ccefc2cc1645eb49
Andrei Sabau <hello@andreisabau.com>
2020-05-10 15:41:23 +0300

    Add Arknav protocol

---
+++ Navtrack.Listener/Protocols/Arknav/ArknavMessageHandler.cs
@@ -0,0 +1,55 @@
+using System.Text.RegularExpressions;
+using Navtrack.Library.DI;
+using Navtrack.Listener.Helpers;
+using Navtrack.Listener.Models;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Arknav
+{
+    [Service(typeof(ICustomMessageHandler<ArknavProtocol>))]
+    public class ArknavMessageHandler : BaseMessageHandler<ArknavProtocol>
+    {
+        public override Location Parse(MessageInput input)
+        {
+            Match locationMatch =
+                new Regex("(\\d{15})," +  // imei
+                          "(.{6})," + // id code
+                          "(\\d{3})," + // status
+                          "(L\\d{3})," + // version
+                          "(A|V)," + // position status
+                          "(\\d+.\\d+),(N|S)," + // latitude dd mm.mmmm N/S
+                          "(\\d+.\\d+),(E|W)," + // longitude ddd mm.mmmm E/W
+                          "(.*?)," + // speed
+                          "(.*?)," + // heading
+                          "(.*?)," + // hdop
+                          "(\\d{2}):(\\d{2}):(\\d{2}) (\\d{2})-(\\d{2})-(\\d{2}),") // hh:mm:ss dd-mm-yy
+                    .Match(input.DataMessage.String);
+
+            if (locationMatch.Groups.Count == 19)
+            {
+                Location location = new Location
+                {
+                    Device = new Device
+                    {
+                        IMEI = locationMatch.Groups[1].Value
+                    },
+                    PositionStatus = locationMatch.Groups[5].Value == "A",
+                    Latitude = GpsUtil.ConvertDmmLatToDecimal(locationMatch.Groups[6].Value,
+                        locationMatch.Groups[7].Value),
+                    Longitude = GpsUtil.ConvertDmmLatToDecimal(locationMatch.Groups[8].Value,
+                        locationMatch.Groups[9].Value),
+                    Speed = locationMatch.Groups[10].Get<decimal?>(),
+                    Heading = locationMatch.Groups[11].Get<decimal?>(),
+                    HDOP = locationMatch.Groups[12].Get<decimal?>(),
+                    DateTime = DateTimeUtil.New(locationMatch.Groups[18].Value, locationMatch.Groups[17].Value,
+                        locationMatch.Groups[16].Value, locationMatch.Groups[13].Value, locationMatch.Groups[14].Value,
+                        locationMatch.Groups[15].Value)
+                };
+
+                return location;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Navtrack.Listener/Protocols/Arknav/ArknavProtocol.cs
@@ -0,0 +1,11 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Arknav
+{
+    [Service(typeof(IProtocol))]
+    public class ArknavProtocol : BaseProtocol
+    {
+        public override int Port => 7031;
+    }
+}
