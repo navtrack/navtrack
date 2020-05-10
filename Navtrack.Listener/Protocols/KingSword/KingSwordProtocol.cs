6651f5a7e535ca340df98a5326c64a124a6ea283
Andrei Sabau <hello@andreisabau.com>
2020-05-10 22:42:04 +0300

    Add KingSword protocol

---
+++ Navtrack.Listener/Protocols/KingSword/KingSwordProtocol.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.KingSword
+{
+    [Service(typeof(IProtocol))]
+    public class KingSwordProtocol : BaseProtocol
+    {
+        public override int Port => 7034;
+        public override byte[] MessageStart => new byte[] {0x2A};
+        public override IEnumerable<byte[]> MessageEnd => new[] {new byte[] {0x23}};
+    }
+}
