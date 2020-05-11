02cb7fa59a7e86e7ca825b2de6bdb226381fe32d
Andrei Sabau <hello@andreisabau.com>
2020-05-12 00:24:39 +0300

    Update GotopProtocol.cs

--- Navtrack.Listener/Protocols/Gotop/GotopProtocol.cs
+++ Navtrack.Listener/Protocols/Gotop/GotopProtocol.cs
@@ -8,7 +8,6 @@ namespace Navtrack.Listener.Protocols.Gotop
     public class GotopProtocol : BaseProtocol
     {
         public override int Port => 7037;
-        public override byte[] MessageStart => new byte[] {0x23};
         public override IEnumerable<byte[]> MessageEnd => new[] {new byte[] {0x23}};
     }
 }
