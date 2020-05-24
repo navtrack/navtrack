1461487390eb7b291f0ca6447f76cec7b16e0e6f
Andrei Sabau <hello@andreisabau.com>
2020-05-24 18:28:47 +0300

    Add Topfly Protocol

---
+++ Navtrack.Listener/Protocols/Topfly/TopflyMessageHandler.cs
@@ -0,0 +1,47 @@
+using System.Text.RegularExpressions;
+using Navtrack.Library.DI;
+using Navtrack.Listener.Helpers;
+using Navtrack.Listener.Helpers.New;
+using Navtrack.Listener.Models;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Topfly
+{
+    [Service(typeof(ICustomMessageHandler<TopflyProtocol>))]
+    public class TopflyMessageHandler : BaseMessageHandler<TopflyProtocol>
+    {
+        public override Location Parse(MessageInput input)
+        {
+            Match locationMatch =
+                new Regex("\\((\\d+)" + // imei
+                          "(.*?)(\\d{12})" + // date time yy mm dd hh mm ss
+                          "(A|V)" + // gps fix
+                          "(\\d+.\\d+)(N|S)" + // latitude
+                          "(\\d+.\\d+)(E|W)" + // longitude
+                          "(\\d{3}.\\d)" + // speed
+                          "(\\d+.\\d+)") // heading
+                    .Match(input.DataMessage.String);
+
+            if (locationMatch.Success)
+            {
+                Location location = new Location
+                {
+                    Device = new Device
+                    {
+                        DeviceId = locationMatch.Groups[1].Value
+                    },
+                    DateTime = NewDateTimeUtil.Convert(DateFormat.YYMMDDHHMMSS, locationMatch.Groups[3].Value),
+                    PositionStatus = locationMatch.Groups[4].Value == "A",
+                    Latitude = GpsUtil.ConvertDmmLatToDecimal(locationMatch.Groups[5].Value, locationMatch.Groups[6].Value),
+                    Longitude = GpsUtil.ConvertDmmLongToDecimal(locationMatch.Groups[7].Value, locationMatch.Groups[8].Value),
+                    Speed = locationMatch.Groups[9].Get<decimal?>(),
+                    Heading = locationMatch.Groups[10].Get<decimal?>()
+                };
+
+                return location;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Navtrack.Listener/Protocols/Topfly/TopflyProtocol.cs
@@ -0,0 +1,12 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.Topfly
+{
+    [Service(typeof(IProtocol))]
+    public class TopflyProtocol : BaseProtocol
+    {
+        public override int Port => 7050;
+        public override byte[] MessageStart => new byte[] {0x23, 0x23};
+    }
+}
