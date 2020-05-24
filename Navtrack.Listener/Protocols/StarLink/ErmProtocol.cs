e102d6b8075ca9b004414b83f79691f91dd18312
Andrei Sabau <hello@andreisabau.com>
2020-05-24 23:16:43 +0300

    Add ERM protocol

---
+++ Navtrack.Listener/Protocols/StarLink/ErmProtocol.cs
@@ -0,0 +1,11 @@
+using Navtrack.Library.DI;
+using Navtrack.Listener.Server;
+
+namespace Navtrack.Listener.Protocols.StarLink
+{
+    [Service(typeof(IProtocol))]
+    public class ErmProtocol : BaseProtocol
+    {
+        public override int Port => 7051;
+    }
+}
