f8f734cb35785aacf098e6b4e72bf81197f5c6b2
Andrei Sabau <hello@andreisabau.com>
2020-05-30 01:35:28 +0300

    Update Dashboard.tsx

--- Navtrack.Core/ClientApp/src/components/home/Dashboard.tsx
+++ Navtrack.Core/ClientApp/src/components/home/Dashboard.tsx
@@ -2,9 +2,18 @@ import React from "react";
 import AdminLayout from "components/framework/layouts/admin/AdminLayout";
 
 export default function Dashboard() {
-  return (
-    <AdminLayout>
-      <div className="bg-white shadow p-3 rounded flex">Welcome to Navtrack!</div>
-    </AdminLayout>
-  );
+    return (
+        <AdminLayout>
+            <div className="bg-white shadow p-3 rounded flex flex-col">
+                <div className="font-bold text-xl">Welcome to Navtrack!</div>
+                <div className="mt-2">
+                    Thank you for registering so early!
+                </div>
+                <div>
+                    Unfortunately Navtrack is not yet officialy and a lot
+                    of things are still not working here, please check back later.
+                </div>
+            </div>
+        </AdminLayout>
+    );
 }
