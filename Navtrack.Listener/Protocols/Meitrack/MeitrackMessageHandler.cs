92a8fef292ac271135c58810e8618e11b1822257
Andrei Sabau <hello@andreisabau.com>
2020-06-20 22:45:11 +0300

    Add extended support for Meitrack protocol

--- Navtrack.Listener/Protocols/Meitrack/MeitrackMessageHandler.cs
+++ Navtrack.Listener/Protocols/Meitrack/MeitrackMessageHandler.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Navtrack.Library.DI;
 using Navtrack.Listener.Helpers;
 using Navtrack.Listener.Models;
@@ -9,28 +10,199 @@ namespace Navtrack.Listener.Protocols.Meitrack
     [Service(typeof(ICustomMessageHandler<MeitrackProtocol>))]
     public class MeitrackMessageHandler : BaseMessageHandler<MeitrackProtocol>
     {
-        public override Location Parse(MessageInput input)
+        public override IEnumerable<Location> ParseRange(MessageInput input)
         {
-            Location location = new Location
-            {
-                Device = new Device
-                {
-                    DeviceId = input.DataMessage.CommaSplit[1]
-                },
-                Latitude = input.DataMessage.CommaSplit.Get<decimal>(4),
-                Longitude = input.DataMessage.CommaSplit.Get<decimal>(5),
-                DateTime = ConvertDate(input.DataMessage.CommaSplit[6]),
-                PositionStatus = input.DataMessage.CommaSplit.Get<string>(7) == "A",
-                Satellites = input.DataMessage.CommaSplit.Get<short>(8),
-                GsmSignal = GsmUtil.ConvertSignal(input.DataMessage.CommaSplit.Get<short>(9)),
-                Speed = input.DataMessage.CommaSplit.Get<decimal?>(10),
-                Heading = input.DataMessage.CommaSplit.Get<decimal?>(11),
-                HDOP = input.DataMessage.CommaSplit.Get<decimal?>(12),
-                Altitude = input.DataMessage.CommaSplit.Get<int>(13),
-                Odometer = input.DataMessage.CommaSplit.Get<uint>(14)
-            };
+            IEnumerable<Location> locations = ParseRange(input, ParseText, ParseBinary_CCC, ParseBinary_CCE);
 
-            return location;
+            return locations;
+        }
+
+        private static IEnumerable<Location> ParseBinary_CCC(MessageInput input)
+        {
+            input.DataMessage.ByteReader.Skip(2); // header
+            input.DataMessage.ByteReader.Skip(1); // data identifier
+            input.DataMessage.ByteReader.Skip(3); // data length
+            input.DataMessage.ByteReader.Skip(1); // ,
+
+            string imei = StringUtil.ConvertByteArrayToString(input.DataMessage.ByteReader.GetUntil(0x2C));
+            string command = StringUtil.ConvertByteArrayToString(input.DataMessage.ByteReader.GetUntil(0x2C));
+
+            if (command == "CCC")
+            {
+                short protocolVersion = input.DataMessage.ByteReader.Get<short>();
+                short dataPackets = input.DataMessage.ByteReader.Get<short>();
+                int cacheRecords = input.DataMessage.ByteReader.Get<int>();
+
+                List<Location> locations = new List<Location>();
+
+                while (input.DataMessage.ByteReader.BytesLeft > 52)
+                {
+                    Location location = new Location
+                    {
+                        Device = new Device
+                        {
+                            DeviceId = imei
+                        }
+                    };
+
+                    byte eventCode = input.DataMessage.ByteReader.GetOne();
+
+                    location.Latitude = input.DataMessage.ByteReader.Get<int>() * 0.000001m;
+                    location.Longitude = input.DataMessage.ByteReader.Get<int>() * 0.000001m;
+                    location.DateTime = new DateTime(2001, 1, 1)
+                        .AddSeconds(input.DataMessage.ByteReader.Get<int>());
+                    location.PositionStatus = input.DataMessage.ByteReader.GetOne() == 0x01;
+                    location.Satellites = input.DataMessage.ByteReader.GetOne();
+                    location.GsmSignal = input.DataMessage.ByteReader.GetOne();
+                    location.Speed = input.DataMessage.ByteReader.Get<short>();
+                    location.Heading = input.DataMessage.ByteReader.Get<short>();
+                    location.HDOP = input.DataMessage.ByteReader.Get<short>() * 0.1m;
+                    location.Altitude = input.DataMessage.ByteReader.Get<short>();
+                    location.Odometer = input.DataMessage.ByteReader.Get<int>();
+                    int runTime = input.DataMessage.ByteReader.Get<int>();
+                    location.MobileCountryCode = input.DataMessage.ByteReader.Get<short>();
+                    location.MobileNetworkCode = input.DataMessage.ByteReader.Get<short>();
+                    location.LocationAreaCode = input.DataMessage.ByteReader.Get<short>();
+                    location.CellId = input.DataMessage.ByteReader.Get<short>();
+
+                    input.DataMessage.ByteReader.Skip(12);
+
+                    locations.Add(location);
+                }
+
+                return locations;
+            }
+
+            return null;
+        }
+
+        private IEnumerable<Location> ParseBinary_CCE(MessageInput input)
+        {
+            input.DataMessage.ByteReader.Skip(2); // header
+            input.DataMessage.ByteReader.Skip(1); // data identifier
+            input.DataMessage.ByteReader.Skip(3); // data length
+            input.DataMessage.ByteReader.Skip(1); // ,
+
+            string imei = StringUtil.ConvertByteArrayToString(input.DataMessage.ByteReader.GetUntil(0x2C));
+            string command = StringUtil.ConvertByteArrayToString(input.DataMessage.ByteReader.GetUntil(0x2C));
+
+            if (command == "CCE")
+            {
+                int cacheRecords = input.DataMessage.ByteReader.Get<int>();
+                short dataPackets = input.DataMessage.ByteReader.Get<short>();
+
+                List<Location> locations = new List<Location>();
+
+                for (int i = 0; i < dataPackets; i++)
+                {
+                    short dataPacketLength = input.DataMessage.ByteReader.Get<short>();
+                    short dataPacketId = input.DataMessage.ByteReader.Get<short>();
+
+                    Location location = new Location
+                    {
+                        Device = new Device
+                        {
+                            DeviceId = imei
+                        }
+                    };
+
+                    int[] sizes = {1, 2, 4};
+
+                    foreach (int size in sizes)
+                    {
+                        byte numberOfPackets = input.DataMessage.ByteReader.GetOne();
+
+                        for (int j = 0; j < numberOfPackets; j++)
+                        {
+                            byte id = input.DataMessage.ByteReader.GetOne();
+
+                            switch (id)
+                            {
+                                case 0x02:
+                                    location.Latitude = input.DataMessage.ByteReader.Get<int>() * 0.000001m;
+                                    break;
+                                case 0x03:
+                                    location.Longitude = input.DataMessage.ByteReader.Get<int>() * 0.000001m;
+                                    break;
+                                case 0x04:
+                                    location.DateTime = new DateTime(2001, 1, 1)
+                                        .AddSeconds(input.DataMessage.ByteReader.Get<int>());
+                                    break;
+                                case 0x05:
+                                    location.PositionStatus = input.DataMessage.ByteReader.GetOne() == 0x01;
+                                    break;
+                                case 0x06:
+                                    location.Satellites = input.DataMessage.ByteReader.GetOne();
+                                    break;
+                                case 0x07:
+                                    location.GsmSignal = input.DataMessage.ByteReader.GetOne();
+                                    break;
+                                case 0x08:
+                                    location.Speed = input.DataMessage.ByteReader.Get<short>();
+                                    break;
+                                case 0x09:
+                                    location.Heading = input.DataMessage.ByteReader.Get<short>();
+                                    break;
+                                case 0x0A:
+                                    location.HDOP = input.DataMessage.ByteReader.Get<short>() * 0.1m;
+                                    break;
+                                case 0x0B:
+                                    location.Altitude = input.DataMessage.ByteReader.Get<short>();
+                                    break;
+                                case 0x0C:
+                                    location.Odometer = input.DataMessage.ByteReader.Get<int>();
+                                    break;
+                                default:
+                                    byte[] value = input.DataMessage.ByteReader.Get(size);
+                                    break;
+                            }
+                        }
+                    }
+
+                    byte numberOfNBytePackets = input.DataMessage.ByteReader.GetOne();
+                    for (int j = 0; j < numberOfNBytePackets; j++)
+                    {
+                        byte id = input.DataMessage.ByteReader.GetOne();
+                        byte size = input.DataMessage.ByteReader.GetOne();
+                        input.DataMessage.ByteReader.Skip(size);
+                    }
+
+                    locations.Add(location);
+                }
+
+                return locations;
+            }
+
+            return null;
+        }
+
+        private static IEnumerable<Location> ParseText(MessageInput input)
+        {
+            if (input.DataMessage.CommaSplit.Length > 14)
+            {
+                Location location = new Location
+                {
+                    Device = new Device
+                    {
+                        DeviceId = input.DataMessage.CommaSplit[1]
+                    },
+                    Latitude = input.DataMessage.CommaSplit.Get<decimal>(4),
+                    Longitude = input.DataMessage.CommaSplit.Get<decimal>(5),
+                    DateTime = ConvertDate(input.DataMessage.CommaSplit[6]),
+                    PositionStatus = input.DataMessage.CommaSplit.Get<string>(7) == "A",
+                    Satellites = input.DataMessage.CommaSplit.Get<short>(8),
+                    GsmSignal = GsmUtil.ConvertSignal(input.DataMessage.CommaSplit.Get<short>(9)),
+                    Speed = input.DataMessage.CommaSplit.Get<decimal?>(10),
+                    Heading = input.DataMessage.CommaSplit.Get<decimal?>(11),
+                    HDOP = input.DataMessage.CommaSplit.Get<decimal?>(12),
+                    Altitude = input.DataMessage.CommaSplit.Get<int>(13),
+                    Odometer = input.DataMessage.CommaSplit.Get<uint>(14)
+                };
+
+                return new[] {location};
+            }
+
+            return null;
         }
 
         private static DateTime ConvertDate(string date) => DateTimeUtil.New(date[..2], date[2..4], date[4..6],
