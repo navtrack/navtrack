ba2fc49275a7117240aecf78383ab510093c502f
Andrei Sabau <hello@andreisabau.com>
2020-08-25 02:44:42 +0300

    Fix modal

--- Navtrack.Core/ClientApp/src/components/library/elements/Modal.tsx
+++ Navtrack.Core/ClientApp/src/components/library/elements/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from "react";
+import ReactDOM from "react-dom";
 
 type Props = {
   children: ReactNode;
@@ -6,21 +7,24 @@ type Props = {
   onContentClick?: () => void;
 };
 
+const modalRoot = document.getElementById("modal-root")!;
+
 export default function Modal(props: Props) {
-  return (
+  return ReactDOM.createPortal(
     <div
       className="fixed left-0 top-0 w-full h-full z-50 flex flex-col items-center justify-center bg-gray-100 fadeIn animated faster"
       style={{ backgroundColor: "rgba(26, 32, 44, 0.15)" }}
       onClick={props.closeModal}>
       <div
         className="shadow rounded p-4 bg-white"
-        onClick={e => {
+        onClick={(e) => {
           e.stopPropagation();
           e.nativeEvent.stopImmediatePropagation();
           if (props.onContentClick) props.onContentClick();
         }}>
         {props.children}
       </div>
-    </div>
+    </div>,
+    modalRoot
   );
 }
