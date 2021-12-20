a17d350195c9897e4366496e865491fd62e6917b
Andrei Sabau <andrei@codeagency.com>
2021-12-20 03:21:12 +0200

    Add docker images action

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,3 +1,3 @@
-docker build -t navtrack/api:latest -f Navtrack.Api/Dockerfile .
-docker build -t navtrack/web:latest -f Navtrack.Web/Dockerfile .
-docker build -t navtrack/listener:latest -f  Navtrack.Listener/Dockerfile .
+docker build -t navtrack/api -f Navtrack.Api/Dockerfile .
+docker build -t navtrack/web -f Navtrack.Web/Dockerfile .
+docker build -t navtrack/listener -f  Navtrack.Listener/Dockerfile .
