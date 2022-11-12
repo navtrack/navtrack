289566e701304d84596dcc7948bb7b85bd98f14b
Andrei Sabau <andrei@codeagency.com>
2022-11-12 03:08:12 +0200

    Add Directory.Build.props

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,3 +1,3 @@
 docker build -t navtrack/api -f Navtrack.Api/Dockerfile .
 docker build -t navtrack/web -f Navtrack.Web/Dockerfile .
-docker build -t navtrack/listener -f  Navtrack.Listener/Dockerfile .
+docker build -t navtrack/listener -f Navtrack.Listener/Dockerfile .
