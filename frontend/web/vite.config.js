d61fcca99b79d4c0754bf09438f9c47f02814635
Andrei Sabău <andrei@codeagency.com>
2023-05-17 00:14:20 +0300

    Update vite.config.js

--- frontend/web/vite.config.js
+++ frontend/web/vite.config.js
@@ -1,10 +1,18 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dns from "dns";
+
+dns.setDefaultResultOrder("verbatim");
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
   return {
+    server: {
+      host: "localhost",
+      port: 3000,
+      open: true
+    },
     define: {
       "process.env": {
         REACT_APP_API_URL: "http://localhost:3001"
@@ -18,7 +26,6 @@ export default defineConfig(() => {
     build: {
       outDir: "build"
     },
-    plugins: [react()],
-    server: {}
+    plugins: [react()]
   };
 });
