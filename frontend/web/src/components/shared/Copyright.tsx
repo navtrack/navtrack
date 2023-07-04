88e18440fc55144d623300ca827bb455722e8574
Andrei Sabău <andrei@codeagency.com>
2023-07-04 03:52:02 +0300

    Add version

--- frontend/web/src/components/shared/Copyright.tsx
+++ frontend/web/src/components/shared/Copyright.tsx
@@ -2,7 +2,11 @@ export function Copyright() {
   return (
     <span>
       © {new Date().getFullYear()}{" "}
-      <a href="https://codeagency.com" target="_blank" rel="noreferrer">
+      <a
+        href="https://codeagency.com"
+        target="_blank"
+        rel="noreferrer"
+        title={import.meta.env.VITE_VERSION}>
         CodeAgency
       </a>
     </span>
--- frontend/web/src/env.d.ts
+++ frontend/web/src/env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
+  readonly VITE_VERSION: string;
 }
 
 interface ImportMeta {
