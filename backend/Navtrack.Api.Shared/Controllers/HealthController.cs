74606c597ad5bfc2fa7b2d907bab1ed982b0a58e
Andrei Sabău <andrei@codeagency.com>
2023-07-04 04:50:16 +0300

    Add version to API

--- backend/Navtrack.Api.Shared/Controllers/HealthController.cs
+++ backend/Navtrack.Api.Shared/Controllers/HealthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Navtrack.Api.Shared.Controllers;
@@ -11,6 +12,8 @@ public class HealthController : ControllerBase
     [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult Get()
     {
-        return Ok("Choco says hi! :)");
+        Version? version = GetType().Assembly.GetName().Version;
+        
+        return Ok($"Choco says hi! :) v{version}");
     }
 }
