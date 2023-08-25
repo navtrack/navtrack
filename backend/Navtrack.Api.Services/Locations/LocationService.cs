8c54ece24e3b22499a20af415ab6763b47042078
Andrei Sabău <andrei@codeagency.com>
2023-08-26 00:05:11 +0300

    Clean up

--- backend/Navtrack.Api.Services/Locations/LocationService.cs
+++ backend/Navtrack.Api.Services/Locations/LocationService.cs
@@ -3,10 +3,8 @@ using System.Threading.Tasks;
 using Navtrack.Api.Model.Locations;
 using Navtrack.Api.Services.Mappers.Locations;
 using Navtrack.Api.Services.Roles;
-using Navtrack.Api.Services.User;
 using Navtrack.DataAccess.Model.Assets;
 using Navtrack.DataAccess.Model.Locations;
-using Navtrack.DataAccess.Model.Users;
 using Navtrack.DataAccess.Services.Assets;
 using Navtrack.DataAccess.Services.Locations;
 using Navtrack.Library.DI;
@@ -19,15 +17,13 @@ public class LocationService : ILocationService
     private readonly IAssetDataService assetDataService;
     private readonly IRoleService roleService;
     private readonly ILocationDataService locationDataService;
-    private readonly ICurrentUserAccessor currentUserAccessor;
 
     public LocationService(IAssetDataService assetDataService, IRoleService roleService,
-        ILocationDataService locationDataService, ICurrentUserAccessor currentUserAccessor)
+        ILocationDataService locationDataService)
     {
         this.assetDataService = assetDataService;
         this.roleService = roleService;
         this.locationDataService = locationDataService;
-        this.currentUserAccessor = currentUserAccessor;
     }
 
     public async Task<LocationListModel> GetLocations(string assetId, LocationFilterModel locationFilter, int page, int size)
@@ -37,8 +33,7 @@ public class LocationService : ILocationService
         roleService.CheckRole(asset, AssetRoleType.Viewer);
 
         List<LocationDocument> locations = await locationDataService.GetLocations(assetId, locationFilter);
-        UserDocument user = await currentUserAccessor.Get();
-
+    
         return LocationListMapper.Map(locations);
     }
 }
