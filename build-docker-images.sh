fde26f212bb47627e71c415c8edac4e26ad6d715
Andrei Sabău <andrei@codeagency.com>
2023-12-22 00:46:58 +0200

    Update build-docker-images.sh

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,3 +1,3 @@
-docker build -t navtrack/api --platform linux/amd64,linux/arm64 -f backend/Navtrack.Api/Dockerfile .
-docker build -t navtrack/web --platform linux/amd64,linux/arm64 -f frontend/Dockerfile frontend
-docker build -t navtrack/listener --platform linux/amd64,linux/arm64 -f backend/Navtrack.Listener/Dockerfile .
+docker buildx build -t navtrack/api --platform linux/amd64,linux/arm64 -f backend/Navtrack.Api/Dockerfile .
+docker buildx build -t navtrack/web --platform linux/amd64,linux/arm64 -f frontend/Dockerfile frontend
+docker buildx build -t navtrack/listener --platform linux/amd64,linux/arm64 -f backend/Navtrack.Listener/Dockerfile .
