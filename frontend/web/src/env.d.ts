b50ff8d8d26d2c59517beeedd1611ac6c4513115
Andrei Sabău <andrei@codeagency.com>
2024-03-11 15:39:40 +0200

    Add configurable env variables to web frontend

--- frontend/web/src/env.d.ts
+++ frontend/web/src/env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly DEV: boolean;
   readonly VITE_API_URL: string;
   readonly VITE_VERSION: string;
+  readonly VITE_LISTENER_HOSTNAME: string;
+  readonly VITE_LISTENER_IP: string;
 }
 
 interface ImportMeta {
