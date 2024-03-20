59269a0993373f7ba7b904579ae17db437aaafe2
Andrei Sabău <andrei@codeagency.com>
2024-03-20 03:58:23 +0200

    Update useCurrentUserQuery.ts

--- frontend/shared/src/hooks/queries/useCurrentUserQuery.ts
+++ frontend/shared/src/hooks/queries/useCurrentUserQuery.ts
@@ -1,8 +1,13 @@
+import { useRecoilValue } from "recoil";
 import { useUserGet } from "../../api/index-generated";
+import { isAuthenticatedAtom } from "../../state/authentication";
 
 export const useCurrentUserQuery = () => {
+  const isAuthenticated = useRecoilValue(isAuthenticatedAtom);
+
   const query = useUserGet({
     query: {
+      enabled: isAuthenticated,
       refetchOnWindowFocus: false,
       refetchOnMount: false
     }
