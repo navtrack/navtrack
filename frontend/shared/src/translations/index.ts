94b1484962aa58bbcd21181a981eefb16c71e5ce
Andrei Sabău <andrei@codeagency.com>
2024-05-10 18:15:48 +0300

    Add EU country list

--- frontend/shared/src/translations/index.ts
+++ frontend/shared/src/translations/index.ts
@@ -7,5 +7,39 @@ export const translations = {
 
 export const countries = countryList;
 
+const euCountryCodes = [
+  "AT",
+  "BE",
+  "BG",
+  "CY",
+  "CZ",
+  "DE",
+  "DK",
+  "EE",
+  "EL",
+  "ES",
+  "FI",
+  "FR",
+  "HR",
+  "HU",
+  "IE",
+  "IT",
+  "LT",
+  "LU",
+  "LV",
+  "MT",
+  "NL",
+  "PL",
+  "PT",
+  "RO",
+  "SE",
+  "SI",
+  "SK"
+];
+
+export const euCountries = countries.filter((x) =>
+  euCountryCodes.includes(x.code)
+);
+
 export const getCountryName = (code?: string) =>
   `${countries.find((x) => x.code === code)?.name}`;
