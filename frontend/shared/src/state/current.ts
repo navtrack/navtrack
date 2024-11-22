c262374057087a7f5272b0a4a7e96ef7c0fa42f9
Andrei Sabău <andrei@codeagency.com>
2024-11-22 18:03:01 +0200

    Update current.ts

--- frontend/shared/src/state/current.ts
+++ frontend/shared/src/state/current.ts
@@ -4,11 +4,17 @@ import { getLocalStorageEffect } from "./getLocalStorageEffect";
 export const currentAssetIdAtom = atom<string | undefined>({
   key: "Navtrack:Current:AssetId",
   default: undefined,
-  effects: [getLocalStorageEffect<string | undefined>()]
+  effects:
+    typeof window === "undefined"
+      ? [getLocalStorageEffect<string | undefined>()]
+      : undefined
 });
 
 export const currentOrganizationIdAtom = atom<string | undefined>({
   key: "Navtrack:Current:OrganizationId",
   default: undefined,
-  effects: [getLocalStorageEffect<string | undefined>()]
+  effects:
+    typeof window === "undefined"
+      ? [getLocalStorageEffect<string | undefined>()]
+      : undefined
 });
