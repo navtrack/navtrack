a81a41ce6eb0d89560f9385a8825f3cb16ebb51c
Andrei Sabău <andrei.sabau@codeagency.com>
2026-06-04 19:48:23 +0300

    Update useGeocodeReverseQuery.ts

--- frontend/shared/src/hooks/queries/geocode/useGeocodeReverseQuery.ts
+++ frontend/shared/src/hooks/queries/geocode/useGeocodeReverseQuery.ts
@@ -4,20 +4,23 @@ import { GeocodeReverseParams } from "../../../api/model";
 type GeocodeReverseQueryProps = GeocodeReverseParams & { isEnabled?: boolean };
 
 export function useGeocodeReverseQuery(props: GeocodeReverseQueryProps) {
-  const query = useGeocodeReverse(props, {
-    query: {
-      queryKey: getGeocodeReverseQueryKey({ lat: props.lat, lon: props.lon }),
-      refetchOnWindowFocus: false,
-      refetchOnMount: false,
-      staleTime: Infinity,
-      enabled:
-        (props.isEnabled || props.isEnabled === undefined) &&
-        props.lat !== undefined &&
-        props.lon !== undefined &&
-        props.lat !== 0 &&
-        props.lon !== 0
+  const query = useGeocodeReverse(
+    { lat: props.lat, lon: props.lon },
+    {
+      query: {
+        queryKey: getGeocodeReverseQueryKey({ lat: props.lat, lon: props.lon }),
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        staleTime: Infinity,
+        enabled:
+          (props.isEnabled || props.isEnabled === undefined) &&
+          props.lat !== undefined &&
+          props.lon !== undefined &&
+          props.lat !== 0 &&
+          props.lon !== 0
+      }
     }
-  });
+  );
 
   return query;
 }
